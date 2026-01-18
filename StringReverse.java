ad0f8372a635f8f7d7db15bb5da65f0515433081
Nisarg <nisarg34.bhatt@gmail.com>
2026-01-18 20:06:33 +0530

    String reversal without using functions

---
+++ StringReverse.java
@@ -0,0 +1,11 @@
+public class StringReverse{
+    public static void main(String[] args){
+        String str="Jello";
+        String reverse="";
+        for(int i=str.length()-1;i>=0;i--){
+            reverse+=str.charAt(i);
+
+        }
+    System.out.println(reverse);
+    }
+}
