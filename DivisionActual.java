00724d5a8db30ddb0a2a94d4ba3c75e8bc39cf76
Nisarg <nisarg34.bhatt@gmail.com>
2026-01-18 19:52:30 +0530

    Divison not by zero

---
+++ DivisionActual.java
@@ -0,0 +1,12 @@
+public class DivisionActual{
+    public static void main(String[] args){
+        int a=10;
+        int b=0;
+        if(b==0){
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+        else{
+            System.out.println("Result="+(a/b));
+        }
+    }
+}
