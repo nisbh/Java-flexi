8bc8ad00e5853c838bf85a88a15b1839b65ff895
Nisarg <nisarg34.bhatt@gmail.com>
2026-01-18 19:40:30 +0530

    Print in different lines

---
+++ printlines.java
@@ -0,0 +1,7 @@
+public class printlines{
+    public static void main(String[] args){
+        System.out.println("Name:Nisarg Bhatt");
+        System.out.println("Roll No:123");
+        System.out.println("Address:Pune,India");
+    }
+}
